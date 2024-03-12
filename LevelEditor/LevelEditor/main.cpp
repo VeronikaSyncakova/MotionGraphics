51388090065e29766253583e506ed8c168bb9a0a
VeronikaSyncakova <veva.syncakova@gmail.com>
2024-03-12 21:56:00 +0000

    editing and playing enabled

--- LevelEditor/LevelEditor/main.cpp
+++ LevelEditor/LevelEditor/main.cpp
@@ -53,7 +53,8 @@ public:
 	}
 	void UpdateUI()
 	{
-		text.setPosition(shape.getPosition().x + 10, shape.getPosition().y + 30);
+		text.setOrigin(text.getGlobalBounds().width / 2.f, text.getGlobalBounds().height / 2.f);
+		text.setPosition(shape.getPosition().x+(shape.getGlobalBounds().width/2.f), shape.getPosition().y+(shape.getGlobalBounds().height/2.f));
 
 		shape.setFillColor(curFillColor);
 	}
@@ -121,6 +122,7 @@ public:
 	sf::RenderWindow* window;
 
 	sf::RectangleShape colors[6];
+	sf::RectangleShape colorsBackg;
 
 	//sf::Texture m_texture;
 	//sf::Sprite m_sprite;
@@ -128,8 +130,10 @@ public:
 	//sf::Sprite m_tiles;
 
 	bool jumping = false;
-	//bool turn = false;
+	bool turn = false;
 	bool reachedEnd = false;
+
+	int chosenTile = 0;
 
 	float velocityX = 0, velocityY = 0, gravity = 0.3;
 
@@ -213,6 +217,7 @@ public:
 				}
 				levelH[row][col].setSize(sf::Vector2f(70, 30));
 				levelH[row][col].setPosition(col * 70, row * 30);
+				levelH[row][col].setOutlineColor(sf::Color::Transparent);
 			}
 		}
 	}
@@ -225,7 +230,10 @@ public:
 			{
 				for (int col = 0; col < numCols; col++)
 				{
-					levelH[row][col].move(-3.7, 0);
+					if (!turn)
+						levelH[row][col].move(-3.7, 0);
+					else
+						levelH[row][col].move(3.7, 0);
 				}
 			}
 		}
@@ -263,6 +271,66 @@ public:
 								break;
 							}
 							else {
+								init();
+							}
+						}
+					}
+
+					if (levelDataH[row][col] == 4)
+					{
+
+						if (playerShape.getGlobalBounds().intersects(levelH[row][col].getGlobalBounds()))
+						{
+							if (playerShape.getPosition().y < levelH[row][col].getPosition().y)
+							{
+								gravity = 0.6f;
+								velocityY = -16.f;
+								jumping = true;
+								playerShape.move(0, -playerShape.getGlobalBounds().height);
+								break;
+							}
+							else {
+								init();
+							}
+						}
+					}
+					if (levelDataH[row][col] == 3)
+					{
+
+						if (playerShape.getGlobalBounds().intersects(levelH[row][col].getGlobalBounds()))
+						{
+							if (playerShape.getPosition().y < levelH[row][col].getPosition().y)
+							{
+								gravity = 0.f;
+								velocityY = 0.f;
+								jumping = false;
+								playerShape.setPosition(playerShape.getPosition().x, levelH[row][col].getPosition().y);
+								playerShape.move(0, -playerShape.getGlobalBounds().height);
+								reachedEnd = true;
+								break;
+							}
+							else {
+								init();
+							}
+						}
+					}
+					if (levelDataH[row][col] == 5)
+					{
+
+						if (playerShape.getGlobalBounds().intersects(levelH[row][col].getGlobalBounds()))
+						{
+							if (playerShape.getPosition().y < levelH[row][col].getPosition().y)
+							{
+								gravity = 0.f;
+								velocityY = 0.f;
+								jumping = false;
+								playerShape.setPosition(playerShape.getPosition().x, levelH[row][col].getPosition().y);
+								playerShape.move(0, -playerShape.getGlobalBounds().height);
+								turn = !turn;
+								break;
+							}
+							else {
+								turn = false;
 								init();
 							}
 						}
@@ -311,10 +379,15 @@ public:
 
 	void editInit()
 	{
+		colorsBackg.setSize(sf::Vector2f(50.f, 190.f));
+		colorsBackg.setFillColor(sf::Color(255, 255, 255, 150));
+		colorsBackg.setPosition(750.f, 0.f);
 		for (int i = 0; i < 6; i++)
 		{
 			colors[i].setSize(sf::Vector2f(35, 25));
 			colors[i].setPosition(800 - 35, 30 * i);
+			colors[i].setOutlineThickness(2.f);
+			colors[i].setOutlineColor(sf::Color::Transparent);
 		}
 		colors[0].setFillColor(sf::Color::Black);
 		colors[1].setFillColor(sf::Color::Red);
@@ -390,25 +463,56 @@ public:
 				}
 			}
 		}
-
+		
+		sf::Vector2f pos = sf::Vector2f(sf::Mouse::getPosition(*window).x, sf::Mouse::getPosition(*window).y);
+		if (pos.x > 700 && pos.y < 180)
+		{
+			for (int i = 0; i < 6; i++)
+			{
+				if (colors[i].getGlobalBounds().contains(pos))
+				{
+					colors[i].setOutlineColor(sf::Color::Yellow);
+					if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+						chosenTile = i;
+				}
+				else if(i!=chosenTile)
+				{
+					colors[i].setOutlineColor(sf::Color::Transparent);
+				}
+			}
+		}
+		
 		for (int row = 0; row < numRows; row++)
 		{
 			for (int col = 0; col < numCols; col++)
 			{
+				if (pos.x >= 750.f && pos.y <= 190.f)
+				{
+				}
+				else if(levelH[row][col].getGlobalBounds().contains(pos))
+				{
+					if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+					{
+						levelDataH[row][col] = chosenTile;
+						editInit();
+					}
+				}
+
 				//tiles[row][col].setPosition(levelH[row][col].getPosition());
 				window->draw(levelH[row][col]);
 				//window.draw(tiles[row][col]);
 			}
 		}
+		window->draw(colorsBackg);
 		for (int i = 0; i < 6; i++)
 		{
 			window->draw(colors[i]);
 		}
+
 	}
 };
 
-enum class GameState { MENU, GAMEPLAY, EXIT, OPTIONS, LEADERBOARD, PAUSE };
-void gameLoop(Game& game);
+enum class GameState { MENU, GAMEPLAY, EXIT, OPTIONS, LEADERBOARD, PAUSE, EDIT };
 
 int main()
 {
@@ -438,7 +542,9 @@ int main()
 	MenuButton MainMenuButtons[4];
 	MenuButton GameBackButton;
 	GameBackButton.Init(sf::Vector2f(0, 0), font, buttonClickSound);
-	GameBackButton.text.setString("Back To Menu");
+	GameBackButton.text.setString("Menu");
+	GameBackButton.text.setCharacterSize(12U);
+	GameBackButton.shape.setSize(sf::Vector2f(70.f, 30.f));
 	sf::Text gameText;
 	gameText.setFont(font);
 	gameText.setString("Coming Soon.....");
@@ -449,9 +555,9 @@ int main()
 
 	}
 	MainMenuButtons[0].text.setString("Play");
-	MainMenuButtons[1].text.setString("Options");
-	MainMenuButtons[2].text.setString("LeaderBoard");
-	MainMenuButtons[3].text.setString("Exit");
+	MainMenuButtons[1].text.setString("Edit");
+	MainMenuButtons[2].text.setString("Exit");
+	MainMenuButtons[3].text.setString("Nothing");
 	sf::RectangleShape creature;
 	creature.setSize(sf::Vector2f(10, 10));
 	creature.setOrigin(5, 5);
@@ -497,18 +603,22 @@ int main()
 				{
 					music.pause();
 					gameState = GameState::GAMEPLAY;
+					game.init();
 				}
 				if (MainMenuButtons[1].pressed == true)
 				{
-					std::cout << " 2 pressed" << std::endl;
+					//std::cout << " 2 pressed" << std::endl;
+					gameState = GameState::EDIT;
+					game.editInit();
 				}
 				if (MainMenuButtons[2].pressed == true)
 				{
-					std::cout << " 3 pressed" << std::endl;
+					window.close();
+					//std::cout << " 3 pressed" << std::endl;
 				}
 				if (MainMenuButtons[3].pressed == true)
 				{
-					std::cout << " 4 pressed" << std::endl;
+					//std::cout << " 4 pressed" << std::endl;
 				}
 
 				window.clear(sf::Color::Black);
@@ -531,11 +641,24 @@ int main()
 					gameState = GameState::MENU;
 				}
 				window.clear(sf::Color::Black);
-
+				game.run();
 				window.draw(GameBackButton.shape);
 				window.draw(GameBackButton.text);
 				//window.draw(gameText);
-				gameLoop(game);
+				window.display();
+			}
+			else if (gameState == GameState::EDIT)
+			{
+				GameBackButton.Update(window);
+				if (GameBackButton.pressed == true)
+				{
+					music.play();
+					gameState = GameState::MENU;
+				}
+				window.clear(sf::Color::Black);
+				game.edit();
+				window.draw(GameBackButton.shape);
+				window.draw(GameBackButton.text);
 				window.display();
 			}
 			timeSinceLastUpdate = sf::Time::Zero;
@@ -543,10 +666,4 @@ int main()
 	}
 
 	return 0;
-}
-
-
-void gameLoop(Game& game)
-{
-	game.edit();
 }
